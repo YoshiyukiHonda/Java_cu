35d1130cdb6fec62a368ba006419368dfaf66910
YoshiyukiHonda <honda19920721@gmail.com>
2023-09-06 23:42:24 +0900

    課題1.3　新規作成

---
+++ HelloWorld_HondaYoshiyuki/src/sample/SelfIntroduction.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		String name = "本田　嘉章";
+		int age = 31;
+		String hobby = "創作物鑑賞";
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
